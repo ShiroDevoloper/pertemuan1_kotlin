fa4112e88e5dc93c582eee38fe6ea0fd0f5d0da7
RPL-24 <malydaauliya@gmail.com>
2022-03-11 16:25:38 +0700

    Percobaan blog dan komentar

---
+++ src/main/kotlin/4_Blok_Komentar.kt
@@ -0,0 +1,13 @@
+fun main() { // ini adalah pembuka blok
+
+    //ini adalah komentar 1 baris
+
+    /*
+    ini adalah komentar multy baris
+    */
+
+    /**
+     * Kotlin Doc --> baris ini untuk mengdokumentasikan kode program
+     * */
+
+}// ini adalah penutup blok
