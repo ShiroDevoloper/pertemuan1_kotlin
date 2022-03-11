5dc30f549ee47d43026eb3143949c2d08c069ecb
RPL-24 <malydaauliya@gmail.com>
2022-03-11 14:28:04 +0700

    Percobaan literal

---
+++ src/main/kotlin/1_Literal.kt
@@ -0,0 +1,16 @@
+fun main() {
+    //Deklarasi dan inisialisasi variable - variable
+    val intLiteral = 5
+    val doubleLiteral = .02
+    val stringLiteral = "Hello"
+    val charLiteral = "1"
+    val booleanLiteral = true
+
+    //tampilkan variable menggunakan sting template
+    println("intLiteral = $intLiteral")
+    println("doubleLiteral = $doubleLiteral")
+    println("stringLiteral = $stringLiteral")
+    println("charLiteral = $charLiteral")
+    println("boolLitteral = $booleanLiteral")
+
+}
