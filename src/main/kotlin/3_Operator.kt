6989e15cffa55e1c3f646462486d1f15c193dd44
RPL-24 <malydaauliya@gmail.com>
2022-03-11 16:22:51 +0700

    Percobaan operator

---
+++ src/main/kotlin/3_Operator.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //Operator
+    //pembuatan operator dasar
+    var a = 1
+    var b = 2
+    println(a+b) //pembuatan operator tambah
+    println(a-b) //pembuatan operator kurang
+    println(a/b) //pembuatan operator kali
+    println(a*b) //pembuatan operator bagi
+    println(a%b) //pembuatan operator hasil bagi
+
+}
