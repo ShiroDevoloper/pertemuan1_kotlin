07028597714667eff13bcb236e6c261496753bf6
RPL-24 <malydaauliya@gmail.com>
2022-03-11 16:11:04 +0700

    Expression and statement

---
+++ src/main/kotlin/2_EkspressionAndStatement.kt
@@ -0,0 +1,8 @@
+fun main() {
+    //Membuat expression dan statement
+    val x = 20 % 4 // contoh expression dan statement
+    println(x) // hasil contoh expression dan statement
+    val y = 30 % 5
+    println(y)
+
+}
