2fec72e4e073d32354ae1ea02bae0c8938fd7d27
RPL-24 <malydaauliya@gmail.com>
2022-03-11 16:56:38 +0700

    Percobaan pembuatan tipe data

--- src/main/kotlin/7_TipeData.kt
+++ src/main/kotlin/7_TipeData.kt
@@ -1,7 +1,31 @@
 fun main() {
     //  penggunaan type data
+
     var a:Byte = 123 // max pengunaan byte adalah 128
+
     var b:Short = 32767 // max pengunaan Short adalah 32768
+
     var c:Int = 11231231 // max pengunaan int adalah (2^31-1)
+
     var d:Long = 12312312312312123 // max pengunaan long adalah (2^63 -1)
+
+    var oneMilion = 1_000_000// garis bawah (underscore) digunakan untuk mempermudah dibaca
+
+    var f:Int = 100
+    var g:Int = 100
+    println(f === g)// hasilnya true karena hasil sama merepresentasikan hasil yang sama
+
+
+    var h:Int =1
+    var i:Long = h.toLong()//contoh konversi eksplisit
+
+    var text = """
+        for (c in "foo")
+        ayam
+    """.trimIndent() //contoh pengguanaan triple quotes
+
+    var j = "ayam"
+    println("ayam $j") // contoh pengguanaan string template
+
+
 }
