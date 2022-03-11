cf96d3a551612f37f4e761984e20e6439aef2348
RPL-24 <malydaauliya@gmail.com>
2022-03-11 16:38:35 +0700

    Percobaan pembuatan tipe data

---
+++ src/main/kotlin/7_TipeData.kt
@@ -0,0 +1,7 @@
+fun main() {
+    //  penggunaan type data
+    var a:Byte = 123 // max pengunaan byte adalah 128
+    var b:Short = 32767 // max pengunaan Short adalah 32768
+    var c:Int = 11231231 // max pengunaan int adalah (2^31-1)
+    var d:Long = 12312312312312123 // max pengunaan long adalah (2^63 -1)
+}
