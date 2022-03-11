e456f55e96b249a91da503ba84f0949a6fa7d37b
RPL-24 <malydaauliya@gmail.com>
2022-03-11 16:33:44 +0700

    Percobaan pembuatan var dan val

---
+++ src/main/kotlin/6_Var vs val.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //var adalah variable yang bisa diubah
+    var a  = 10
+    a = a + 1
+    println(a)
+
+    //var adalah variable yang tidak bisa diubah
+    val c = 1
+    // c = c + 1 hasil error karena variable nya tida bisa diubah
+    println(c)
+}
